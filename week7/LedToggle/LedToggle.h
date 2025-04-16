ac657a2a46e4a4afde07d6199bfa491a0ad4006f
dyd <162683643+Jojun05@users.noreply.github.com>
2025-04-16 21:57:46 +0900

    Update LedToggle.h

--- week7/LedToggle/LedToggle.h
+++ week7/LedToggle/LedToggle.h
@@ -1,15 +1,24 @@
 #ifndef LedToggle_h
 #define LedToggle_h
 #include "Arduino.h"
+// LedToggle 클래스 선언
+// LED를 제어하고 상태 전환기능
 class LedToggle{
+
     public:
+//LED 핀을 설정 초기 상태를 설정
       LedToggle(int pin);
+//LED 핀과 상태전환 딜레이 시간을 설정
       LedToggle(int pin,unsigned long delayTime);
+//LED 상태 전환메소드
     void toggle();
 
     private:
+//LED핀번호 저장변수
         int _pin;
+//LED현재 상태 저장
         bool _state;
+//딜레이 시간 저장
         unsigned long _delayTime;
 };
 #endif
