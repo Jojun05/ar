0381f9e006356926d32582715d570d6df5412de8
dyd <162683643+Jojun05@users.noreply.github.com>
2025-04-16 21:59:21 +0900

    Update myled.ino

--- week7/myled.ino
+++ week7/myled.ino
@@ -1,8 +1,11 @@
 #include <LedToggle.h> //Sketch - include library
 LedToggle led(LED_BUILTIN);
-
+// LedToggle 객체를 생성
 void setup(){
+  // setup()  초기 설정
 }
 void loop(){
+  // Arduino의 메인 루프 계속 밤복 실행됨
+  //LED 계속 전환 켜지고 꺼지고
   led.toggle();
 }
